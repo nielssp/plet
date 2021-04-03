a7835417e17d5e8bab1fb2214afd6927eec841d0
Niels Sonnich Poulsen <nielssonnich@gmail.com>
2021-04-03 19:35:57 +0200

    Improve syntax highlighting in docs.

--- doc/pygments/lexers.py
+++ doc/pygments/lexers.py
@@ -1,5 +1,6 @@
 from pygments.lexer import *
 from pygments.token import *
+from pygments.lexers.html import HtmlLexer
 
 class TscLexer(RegexLexer):
     name = 'TSC'
@@ -8,6 +9,9 @@ class TscLexer(RegexLexer):
 
     tokens = {
         'root': [
+            include('command'),
+        ],
+        'command': [
             (r'\{\#.*?\#\}', Comment.Multiline),
             (r'#.*?\n', Comment.Single),
             (r'(if|then|else|for|in|switch|case|default|end|and|or|not|do|export|return|break|continue)\b', Keyword),
@@ -15,54 +19,57 @@ class TscLexer(RegexLexer):
             (r"'(\\\\|\\[^\\]|[^'\\])*'", String.Single),
             (r"\b[0-9]+\b", Number),
             (r"\b[a-zA-Z_][a-zA-Z0-9_]*\b", Name),
-            (r'\+|-|\*|/|<=|>=|==|!=|<|>|=', Operator),
+            (r'\+|-|\*|/|<=|>=|==|!=|<|>|=|\?', Operator),
             (r'[\.,:|]', Punctuation),
             (r'\[', Punctuation, 'array'),
             (r'\(', Punctuation, 'expression'),
             (r'\{', Punctuation, 'statements'),
-            (r'\}', Punctuation, 'template'),
+            (r'\}', Comment.Preproc, 'template'),
             (r'"', String, 'string'),
             ('\s+', Whitespace),
         ],
         'template': [
             (r'[^{]+', String),
             (r'\{\#.*?\#\}', Comment.Multiline),
-            (r'\{', Text, '#pop'),
+            (r'\{', Comment.Preproc, '#pop'),
         ],
         'string': [
             (r'[^"{]+', String),
             (r'\{\#.*?\#\}', Comment.Multiline),
-            (r'\{', Punctuation, 'statements'),
+            (r'\{', Comment.Preproc, 'statements'),
             ('"', String, '#pop'),
         ],
         'statements': [
-            (r'\}', Punctuation, '#pop'),
-            include('root'),
+            (r'\}', Comment.Preproc, '#pop'),
+            include('command'),
         ],
         'array': [
             (r'\]', Punctuation, '#pop'),
-            include('root'),
+            include('command'),
         ],
         'expression': [
             (r'\)', Punctuation, '#pop'),
-            include('root'),
+            include('command'),
         ],
     }
 
-class TscTemplateLexer(RegexLexer):
-    name = 'TSC Template'
-    aliases = ['tsc-template']
-    filenames = ['*.html.tss']
+class TxtTscLexer(TscLexer):
+    name = 'TXT+TSC'
+    aliases = ['txt+tsc']
+    filenames = ['*.txt.tss']
 
     tokens = {
         'root': [
             (r'[^{]+', Other),
             (r'\{\#.*?\#\}', Comment.Multiline),
-            (r'\{', Text, 'statements'),
-        ],
-        'statements': [
-            (r'(.+?)(\})',
-                bygroups(using(TscLexer), Name.Text),
-                '#pop'),
+            (r'\{', Comment.Preproc, 'statements'),
         ],
     }
+
+class HtmlTscLexer(DelegatingLexer):
+    name = 'HTML+TSC'
+    aliases = ['html+tsc']
+    filenames = ['*.html.tss']
+
+    def __init__(self, **options):
+        super().__init__(HtmlLexer, TxtTscLexer, **options)
